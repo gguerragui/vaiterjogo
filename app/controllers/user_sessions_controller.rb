8b2c94e486629395cad1fa99ade782a22e426e16
Guilherme Guerra <gguerragui@gmail.com>
2010-07-15 01:33:47 -0300

    Controle de sessão

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Usuário logado com sucesso."
-      redirect_to root_url
+      redirect_to users_path
     else
       render :action => 'new'
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = current_user
+  end
+
   def new
     @user = User.new
   end
