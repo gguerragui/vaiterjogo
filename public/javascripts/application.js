e55bd2f1054c94ca9f83064ebcc3a5212d7af5a0
jeanmdias <jeanmdias@gmail.com>
2010-08-02 21:38:53 -0300

    jCarousel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,37 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function mycarousel_initCallback(carousel) {
+	jQuery('.jcarousel-control a').bind('click', function() {
+		carousel.scroll(jQuery.jcarousel.intval(jQuery(this).text()));
+		return false;
+	});
+
+	jQuery('#mycarousel-next').bind('click', function() {
+		carousel.next();
+		return false;
+	});
+
+	jQuery('#mycarousel-prev').bind('click', function() {
+		carousel.prev();
+		return false;
+	});
+};
+
 $(document).ready(function() {
 
-	$('label').cluetip({
+	jQuery("#mycarousel").jcarousel({
+		scroll: 1,
+		auto: 5,
+		wrap: 'circular',
+		initCallback: mycarousel_initCallback, 
+		buttonNextHTML: null, 
+		buttonPrevHTML: null
+	});
+	
+	$('.sports a').cluetip({
 		dropShadow: false, 
 		width: '150px',
-		//height: '150px', 
 		sticky: true,
 		mouseOutClose: true,
 		closeText: '',
@@ -15,4 +40,5 @@ $(document).ready(function() {
 		leftOffset:-85, 
 		fx: { open: 'fadeIn', openSpeed: '3' }
 	});
+	
 });
